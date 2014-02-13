7eb7bc834666495a1838bdbb8b684a59490248c6
thansen <thansen@solire.fr>
2014-02-13 16:22:46 +0100

    Debug initialSlide option

--- jquery.fullPage.js
+++ jquery.fullPage.js
@@ -10,7 +10,7 @@
 	$.fn.fullpage = function(options) {
 		// Create some defaults, extending them with any options that were provided
 		options = $.extend({
-            'initalSlide' : 0,
+			'initialSlide' : 0,
 			"verticalCentered" : true,
 			'resize' : true,
 			'slidesColor' : [],
